e7140ae2b377b4e4dac528a919a0836812e10e87
Rob <Zhouenkissinger@gmail.com>
2019-12-25 14:21:04 -0600

    Now saves progress in the script to allow persistent use of the script, and has a rudimentary error catcher.

--- QC_Scraper.py
+++ QC_Scraper.py
@@ -2,58 +2,103 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import sys
+import shelve
 
 
 
 def QC_page_finder():
     os.makedirs('QC', exist_ok = True)
-    url = 'https://questionablecontent.net/view.php?comic=1'
+    err_list = []
     base_url = 'https://questionablecontent.net'
 
-    err_list = []
-    page_count = 1
+    QC_tracker = shelve.open('QC_tracker', writeback = True)
 
+    if 'page_count' in QC_tracker:
+
+        page_count = QC_tracker['page_count']
+        url = f'https://questionablecontent.net/view.php?comic={page_count}'
+        sys.stdout.write(f'Page count == {page_count}, URL == {url}.')
+        sys.stdout.flush()
+
+        try:
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html5lib')
+            img = soup.find_all(id = 'strip')
+            next_link = img[0].parent.get('href')
+            if next_link.endswith('#'):
+                sys.stdout.write('No new comics since this was last run.')
+                sys.stdout.flush()
+                return 'DONE!'
+        except:
+            sys.stdout.write('Not able to reach the QC server. Aborting operation!\n')
+            sys.stdout.flush()
+            return 'DONE!'
+
+    else:
+
+        url = 'https://questionablecontent.net/view.php?comic=1'
+        page_count = 1
+
+        try:
+            test = requests.get(url)
+
+        except:
+            sys.stdout.write('Not able to reach the QC server. Aborting operation!\n')
+            sys.stdout.flush()
+            return 'DONE!'
 
     while not url.endswith('#'):
 
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html5lib')
-        
-        img = soup.find_all(id = 'strip')
-        img_info = img[0].get('src')
-        next_link = img[0].parent.get('href')
-        img_link = f'{base_url}{img_info[1:]}'
-        
+        try:
 
-        if len(img) != 1:
-            err_list.append(page_count)
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html5lib')
             
-        else:
-            image = requests.get(img_link)
-            sys.stdout.write('\n')
-            sys.stdout.write(f'Downloading comic number {page_count}')
+            img = soup.find_all(id = 'strip')
+            img_info = img[0].get('src')
+            next_link = img[0].parent.get('href')
+            img_link = f'{base_url}{img_info[1:]}'
+            
+            if len(img) != 1:
+                err_list.append(page_count)
+                
+            else:
+                image = requests.get(img_link)
+                sys.stdout.write('\n')
+                sys.stdout.write(f'Downloading comic number {page_count}')
+                sys.stdout.flush()
+                comname = f'QC{str(page_count).zfill(4)}{img_link[-4:]}'
+                imageFile = open(os.path.join('QC', os.path.basename(comname)), 'wb')
+                for chunk in image.iter_content(100_000):
+                    imageFile.write(chunk)
+                imageFile.close()
+
+        except:
+            sys.stdout.write(f'Error in downloading comic number {page_count}. Saving progress and aborting process.\n')
             sys.stdout.flush()
-            comname = f'QC{str(page_count).zfill(4)}{img_link[-4:]}'
-            imageFile = open(os.path.join('QC', os.path.basename(comname)), 'wb')
-            for chunk in image.iter_content(100_000):
-                imageFile.write(chunk)
-            imageFile.close()
-
-
-
+            url = f'https://questionablecontent.net/view.php?comic={page_count}'
+            QC_tracker['page_count'] = page_count
+            QC_tracker['url'] = url
+            QC_tracker.close()
+            return 'Error!'
         
         url = f'{base_url}/{next_link}'
-        
-
+      
         page_count += 1
 
-    print(f'Errors were found downloading these comics: {err_list}')
+    QC_tracker['page_count'] = page_count
+    QC_tracker['url'] = url
+    QC_tracker.close()
 
+    sys.stdout.write('All done!')
+    sys.stdout.flush()
 
+    if len(err_list) > 1 :
+        sys.stdout.write(f'Errors were found downloading these comics: {err_list}')
+        sys.stdout.flush()
 
-QC_page_finder()
+def main():
+    QC_page_finder()
 
-
-
-
-
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 
 This script downloads every strip from the Questionable Content webcomic, from the first one to whichever is the most current, puts them into an accompanying directory, and numbers them in order automatically.
 
-It will also print out any strips that it had errors downloading, although on the test run that seemed to produce no errors.
+UPDATE: This will now save its progress in downloading QC so that you can continuously update your directory with new QC comics. 
+
+There is an error catcher to flag a page without comics and a way to keep track of progress in case you lose internet connection during a run of the scraper. 
 
 # A note
 
-If you're going to do this, and can afford to do so, consider buying something from the QC store: https://topatoco.com/collections/jeph-jacques They sell good stuff, and if you enjoy the strip so much you want every image from the strip it's worth supporting them financially.
+If you're going to do this, and can afford to do so, you should absolutely give material support to the Jeph Jacques and the QC endeavour.  You can find the store [here](https://topatoco.com/collections/jeph-jacques) which sells great stuff, and you can find his patreon [here](https://www.patreon.com/jephjacques). Web-comics, and creative arts in general, are a precarious endeavour and you should support the people who do it however you can. If you can't, I understand, but if you ever can please try to do so. 
 
 # Future plans
 
-Probably not going to touch this again. 
+I originally said I wouldn't update this, but I have. I may do it again, but I think it has all the functionality it needs to work. 
 
 Enjoy!
